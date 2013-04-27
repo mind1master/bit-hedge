9a92810bff02eb4d0c3c97efb8b0c282902baff1
Sergiy Karpenko <sergiy@transferwise.com>
2013-04-27 13:47:36 +0300

    Dummy rate and premium methods

--- bit_hedge/apps/core/utils.py
+++ bit_hedge/apps/core/utils.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
 # put misc functions that are used in models, views, tags, etc.
+
+# USD to BTC
+def getRate() :
+    rate = 0.139
+    return rate
+
+def getPremium(rate, date, amount) :
+    #TODO
+    return amount * 0.035
